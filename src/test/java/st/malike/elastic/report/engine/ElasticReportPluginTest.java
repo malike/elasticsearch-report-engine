f5083b6b54aab5e8a90fdbff387e82d7f05a7f4c
malike <st.malike@gmail.com>
2017-06-18 13:19:21 +0000

    initial setup for plugin

---
+++ src/test/java/st/malike/elastic/report/engine/ElasticReportPluginTest.java
@@ -0,0 +1,37 @@
+package st.malike.elastic.report.engine;
+
+import org.elasticsearch.action.admin.cluster.node.info.NodeInfo;
+import org.elasticsearch.action.admin.cluster.node.info.NodesInfoResponse;
+import org.elasticsearch.plugins.Plugin;
+import org.elasticsearch.plugins.PluginInfo;
+import org.elasticsearch.test.ESIntegTestCase;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import static org.hamcrest.Matchers.is;
+
+/**
+ * malike_st.
+ */
+public class ElasticReportPluginTest extends ESIntegTestCase {
+
+    @Override
+    protected Collection<Class<? extends Plugin>> nodePlugins() {
+        return Collections.singleton(ElasticReportPlugin.class);
+    }
+
+    public void testPluginIsLoaded() throws Exception {
+        NodesInfoResponse response = client().admin().cluster().prepareNodesInfo().setPlugins(true).get();
+        for (NodeInfo nodeInfo : response.getNodes()) {
+            boolean pluginFound = false;
+            for (PluginInfo pluginInfo : nodeInfo.getPlugins().getPluginInfos()) {
+                if (pluginInfo.getName().equals(ElasticReportPlugin.class.getName())) {
+                    pluginFound = true;
+                    break;
+                }
+            }
+            assertThat(pluginFound, is(true));
+        }
+    }
+}
