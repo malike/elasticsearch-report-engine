29a9f373fa6dfd37787bca8addcd0190bf8d6de3
malike <st.malike@gmail.com>
2018-02-28 23:39:53 +0000

    file proxy for report files from file system

--- src/main/java/st/malike/elastic/report/engine/service/FileServer.java
+++ src/main/java/st/malike/elastic/report/engine/service/FileServer.java
@@ -1,7 +1,16 @@
 package st.malike.elastic.report.engine.service;
 
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+
 /**
  * @autor malike_st
  */
-public class FileServer {
+public class FileServer implements HttpHandler {
+    @Override
+    public void handle(HttpExchange httpExchange) throws IOException {
+
+    }
 }
---
+++ src/test/java/st/malike/elastic/report/engine/service/FileServerTest.java
@@ -0,0 +1,7 @@
+package st.malike.elastic.report.engine.service;
+
+/**
+ * @autor malike_st
+ */
+public class FileServerTest {
+}
