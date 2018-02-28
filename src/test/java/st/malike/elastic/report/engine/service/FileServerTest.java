e983cca906479f76f15811407f7b506bb972232f
malike <st.malike@gmail.com>
2018-02-28 23:56:37 +0000

    file proxy for report files from file system

--- src/test/java/st/malike/elastic/report/engine/service/FileServerTest.java
+++ src/test/java/st/malike/elastic/report/engine/service/FileServerTest.java
@@ -1,7 +1,23 @@
 package st.malike.elastic.report.engine.service;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
 /**
  * @autor malike_st
  */
+@RunWith(MockitoJUnitRunner.class)
 public class FileServerTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testSomeMethod() throws Exception {
+
+    }
 }
